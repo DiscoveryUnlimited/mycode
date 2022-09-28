ac1fdf84fcd12017b51fbf735483578fa0ff74ef
DiscoveryUnlimited <jason.d.pagan89@gmail.com>
2022-09-28 21:58:39 +0000

    advanced lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | JPagan
+   List - simple list"""
+
+def main():
+    # List of animals
+    list = ["Dog", "Cat", "Fox", "Hog", "Cow"]
+
+    # display list of animals
+    print(list[0], list[1], list[2], list[3], list[4], sep=" ")
+
+main()
+
