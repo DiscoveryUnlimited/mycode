ae49af78aebee0bf1b48aa898c6824d1cd4cca81
DiscoveryUnlimited <jason.d.pagan89@gmail.com>
2022-09-28 19:11:08 +0000

    input function challenge

---
+++ lab_input/challenge.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# below is a function containing our code
+def main():
+
+    # input asking the user's name
+    name_input = input("Please enter your name: ")
+    
+    #input asking what day of the week it is
+    day_input = input("Please enter the day of the week: ")
+
+    # display statement
+    print("Hello, "+ name_input + "! Happy " + day_input + "!")
+    
+# call main
+main()
