75886a306f159830bc2e8f33e89ba5e6ac1d1aa5
DiscoveryUnlimited <jason.d.pagan89@gmail.com>
2022-09-28 22:22:17 +0000

    update

--- advlist/animal-list.py
+++ advlist/animal-list.py
@@ -7,7 +7,7 @@ def main():
     list = ["Dog", "Cat", "Fox", "Hog", "Cow"]
 
     # display list of animals
-    print(list[0], list[1], list[2], list[3], list[4], sep=" ")
+    print(" ".join(list))
 
 main()
 
