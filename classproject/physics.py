daa54c0ed3252d68457f47882411448986b8c42f
DiscoveryUnlimited <jason.d.pagan89@gmail.com>
2022-10-04 14:19:28 +0000

    Initialize physics project

---
+++ classproject/physics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Research | JPagan
+Calculates magnetic fields"""
+
+
+def main():
+
+    print("Magnetic Field Calculator")
+
+if __name__ == "__main__":
+    main()
